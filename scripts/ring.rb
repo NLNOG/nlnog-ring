e6354e813edabf5e1ba3ec58066e01a8d24eb486
Saku Ytti <saku@ytti.fi>
2011-12-05 13:32:24 +0200

    refactoring, ring-uping
    ring-all and ring-uping now use library ring.rb, which should be
    installed in /usr/local/lib/site_ruby
    
    ring-all is not using popen3 and forking openssh, instead it is using
    pureruby ssh, as this was only way to get return value for commands ran
    
    ring-all also got ansi colours and per default outputs to lses
    
    ring-uping is unidirectional ping, but I'm unsure yet if it's useful and
    if I'm going to maintain it

---
+++ scripts/ring.rb
@@ -0,0 +1,125 @@
+#!/usr/bin/env ruby
+
+
+module Ring
+require 'etc'
+Domain  = 'ring.nlnog.net'
+User    = Etc.getlogin
+Date    = Time.now.utc.strftime '%Y-%m-%d %H:%M:%S (UTC)'
+NodeCMD = "dig #{Ring::Domain} txt +short +tcp|tr -d '\"'|tr ' ' '\n'"
+Pager   = '/usr/bin/less -R'
+SSHCMD  = 'ssh -q -t -C '
+
+class SSH
+  require 'net/ssh'
+  # takes hostname, command, output string object and optionally usernme as input
+  # output is taken as string, instead of returned, so that partial output can be captured
+  # returns exit status and timestamp
+ 
+  attr_reader :exit_status, :time
+  def self.run node, cmd, output=[], user=Ring::User, &block
+    ssh = new node, cmd, output, user, &block
+    [ssh.exit_status, ssh.time]
+  end
+  def initialize node, cmd, output, user, &block
+    stderr, stdout = '', ''
+    @exit_status = 0
+    # TODO: Can't use compression, as if connection breaks middle of reading (fail2ban) we may get
+    # 'zlib(finalizer): the stream was freed prematurely.' from zlib to stderr. 
+    Net::SSH.start(node, user, {:auth_methods=>%w(publickey), :compression=>false}) do |ssh|
+      ssh.open_channel do |ch|
+        ch.exec(cmd) do |ch, ack|
+          ch.on_data                   { |ch, data| ssh_read data, stdout, :output=>output, :mode=>:stdout, &block}
+          ch.on_extended_data          { |ch, type, data| ssh_read data, stderr, :output=>output, :mode=>:stderr, &block}
+          ch.on_request('exit-status') { |ch, data| @exit_status += data.read_long }
+          ch.on_request('exit-signal') { |ch, data| @exit_status += (data.read_long << 8) }
+        end
+        ch.wait
+      end
+      ssh.loop
+    end
+    @time = Time.now
+  end
+
+  private
+
+  # reads ssh output one line at a time, possibly we could allow returning partial lines too
+  def ssh_read source, destination, opt={}, &block
+    output = opt[:output] || ''
+    mode =   opt[:mode]   || :stdout
+    destination << source
+    lines = destination.to_a
+    destination.replace ''
+    while line = lines.shift
+      if line[-1..-1] == "\n"
+        if block_given?
+          yield [line, mode]
+        else
+          output << [line, mode]
+        end
+      else
+        destination.replace line
+      end
+    end
+  end
+end
+
+class Open
+require 'open3'
+  def self.ssh node, cmd, output
+    cmd = Ring::SSHCMD + [node, cmd].join(' ')
+    popen3 cmd, output
+  end
+  def self.popen3 cmd, output
+    exit_status = 0
+    Open3.popen3(cmd) do |stdin, stdout, stderr, waitth|
+      while line_err = stderr.gets or line_in = stdout.gets
+        output << [line_err, :stderr] if line_err
+        output << [line_in, :stdout] if line_in
+      end
+      #exit_status = waitth.value #this needs ruby1.9
+    end
+    [exit_status, Time.now]
+  end
+end
+
+class Lock
+  def initialize lock=true
+    @lock = lock
+  end
+  def unlock
+    @lock = false
+  end
+  def lock
+    @lock = true
+  end
+  def locked?
+    @lock
+  end
+end
+
+# returns array of ring node hostnames
+def self.nodes
+  ## TODO: make it pure ruby, once ring has new enough resolv module with TCP support
+  %x(#{Ring::NodeCMD}).split "\n"
+end
+
+# replace myself with pager, and run original in child process whose input is directed to mum
+def self.pager
+  read, write = IO.pipe
+
+  unless fork
+    $stdout.reopen(write)
+    $stderr.reopen(write) if $stderr.tty?
+    read.close
+    write.close
+    return
+  end
+
+  $stdin.reopen(read)
+  read.close
+  write.close
+
+  exec Ring::Pager
+end
+end
