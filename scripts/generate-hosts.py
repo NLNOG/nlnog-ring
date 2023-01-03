09fb40f077af167ba61e8952fd25396958b44453
Martin Pels <martin@rodecker.nl>
2023-01-03 10:42:11 +0100

    generate-hosts.py to python3

--- scripts/generate-hosts.py
+++ scripts/generate-hosts.py
@@ -1,25 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Written by Job Snijders <job@snijders-it.nl>
 
 import sys
 import dns.resolver
 
 ringdomain = "ring.nlnog.net"
+hosts = []
+all_hosts = []
 
-def collect_txt_record():
-    answers = dns.resolver.query(ringdomain, 'TXT')
-    text = str(answers[0])
-    if text and len(text) > 50 and ("xlshosting01" in text) and ("intouch01" in text):
-        text = text[1:-1]
-    else:
-        sys.exit("error: we probably didn't receive a full txt record")
-    return text
-
-record = collect_txt_record()
+for answers in dns.resolver.query(ringdomain, 'TXT',):
+    hosts.append(answers.to_text()[1:-1].split(' '))
+all_hosts = sum(hosts, [])
 
 # print standard header that every hosts needs regardless
 
-print """127.0.0.1   localhost
+print("""127.0.0.1   localhost
 
 # The following lines are desirable for IPv6 capable hosts
 ::1     localhost ip6-localhost ip6-loopback
@@ -28,16 +23,18 @@ ff00::0 ip6-mcastprefix
 ff02::1 ip6-allnodes
 ff02::2 ip6-allrouters
 
-212.19.220.59   master01 master01.ring.nlnog.net puppet
-2001:6e0:100:4001::3    master01 master01.ring.nlnog.net puppet
-"""
+78.152.42.69 master01 master01.infra..ring.nlnog.net puppet
+2a02:d28:666::69    master01 master01.infra.ring.nlnog.net puppet
 
-servers = record.split(" ")
+""")
 
-for server in servers:
+for server in all_hosts:
     server_fqdn = server + "." + ringdomain
     for record in 'A', 'AAAA':
-        server_record = dns.resolver.query(server_fqdn, record)
-        server_record = str(server_record[0])
-        print server_record + "\t" + server + "\t" + server_fqdn
+        try:
+            server_record = dns.resolver.query(server_fqdn, record)
+            server_record = str(server_record[0])
+            print(server_record + "\t" + server + "\t" + server_fqdn)
+        except Exception as e:
+            sys.stderr.write("{} record not found for {}: {}\n".format(record,server_fqdn,str(e)))
     print
